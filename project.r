970ba1e581ebaa61562da51a727d3c51c8020629
Devon Diffie <devon.m.diffie@gmail.com>
2023-04-17 15:03:58 -0600

    added code for project

--- project.r
+++ project.r
@@ -1,96 +1,129 @@
-######Data Preprocessing (Descriptive Method - Clustering):
+install.packages("readr") # Install the readr package
+library(readr) # Load the readr package
+install.packages("forecast")
+###############################################################
+######Data Preprocessing (Descriptive Method - Clustering):####
+###############################################################
 # Load required libraries
 library(dplyr)
 library(tidyr)
 library(readr)
+library(cluster)
+library(caret)
+library(forecast)
+library(ggplot2)
+library(e1071)
+library(rpart)
+library(rpart.plot)
+library(rpart)
+library(rpart.plot)
 
 # Load the data
-data <- read_csv("europearibnbinfo.csv")
+data <- read_csv("projectdataset.csv", col_types = cols(
+  City = col_character(),
+  Price = col_double(),
+  Day = col_character(),
+  `Room Type` = col_character(),
+  `Shared Room` = col_logical(),
+  `Private Room` = col_logical(),
+  `Person Capacity` = col_double(),
+  Superhost = col_logical(),
+  `Multiple Rooms` = col_double(),
+  Business = col_double(),
+  `Cleanliness Rating` = col_double(),
+  `Guest Satisfaction` = col_double(),
+  Bedrooms = col_double(),
+  `City Center (km)` = col_double(),
+  `Metro Distance (km)` = col_double(),
+  `Attraction Index` = col_double(),
+  `Normalised Attraction Index` = col_double(),
+  `Restraunt Index` = col_double()
+))
 
-# Perform data cleaning - handle missing values
-data <- na.omit(data)
+# Convert data types
+data$City <- as.factor(data$City)
+data$Day <- as.factor(data$Day)
+data$`Room Type` <- as.factor(data$`Room Type`)
+data$`Shared Room` <- as.logical(data$`Shared Room`)
+data$`Private Room` <- as.logical(data$`Private Room`)
+data$Price <- as.numeric(data$Price)
+data$`Cleanliness Rating` <- as.numeric(data$`Cleanliness Rating`)
 
-# Perform data cleaning - correct data types if needed
-
-# Perform data cleaning - handle outliers or inconsistencies if needed
-
-# Perform data cleaning - other data preprocessing steps if needed
-
-
-######Data Exploration and Visualization (Descriptive Method - Clustering):
-# Conduct exploratory data analysis (EDA)
-summary(data)  # Summary statistics of the data
-head(data)     # Preview of the first few rows of data
-str(data)      # Structure of the data
-
-# Visualize the data using various charts and graphs
-# For example, create bar charts, scatter plots, or heatmaps to explore patterns and distribution in the data
-
-
-
-######Clustering (Descriptive Method - Clustering):
-# Use clustering techniques, such as k-means or hierarchical clustering, to group the data
-# Based on location, room type, host versus super host, and other relevant variables
-
-# Example using k-means clustering
-library(cluster)
-
-# Select relevant variables for clustering
-cluster_vars <- data %>%
-  select(location, room_type, host_vs_superhost)
-
-# Scale the variables if needed
-cluster_vars_scaled <- scale(cluster_vars)
-
-# Perform k-means clustering
+# Perform K-means clustering
 k <- 3  # Number of clusters
-kmeans_model <- kmeans(cluster_vars_scaled, centers = k, nstart = 10)
-cluster_assignments <- kmeans_model$cluster
+set.seed(123)  # Set seed for reproducibility
+data_cluster <- data[, c("Price", "Person Capacity", "Cleanliness Rating", "Bedrooms", "City Center (km)", "Attraction Index", "Restraunt Index")]  # Select relevant columns for clustering
+kmeans_model <- kmeans(scale(data_cluster), centers = k, nstart = 10)  # Perform K-means clustering
 
-# Add cluster assignments to the original data
-data$cluster <- cluster_assignments
+# Add cluster labels to original data
+data$Cluster <- as.factor(kmeans_model$cluster)
 
+# Scatter plot of Price vs. City Center (km) with cluster labels
+ggplot(data, aes(x = Price, y = `City Center (km)`, color = Cluster)) +
+  geom_point() +
+  labs(title = "K-means Clustering of Accommodation Data",
+       x = "Price",
+       y = "City Center (km)") +
+  theme_minimal()
 
+#############################################
+###############Classification################
+#############################################
 
-######Feature Engineering (Predictive Method - Classification):
-# Create relevant features or variables that can be used in the predictive models
-# For example, calculate average ratings, total number of reviews, or any other relevant metrics
+# Create a copy of the original data
+data_classification <- data
 
-# Example of creating average rating feature
-data <- data %>%
-  group_by(cluster) %>%
-  mutate(avg_rating = mean(rating, na.rm = TRUE))
-
-
-
-######Classification (Predictive Method - Classification):
-# Build classification models, such as logistic regression, decision trees, or random forests
-# To predict the likelihood of high satisfaction ratings based on the clusters created in the previous step
-
-# Example using logistic regression
-library(glmnet)
-
-# Select relevant variables for the classification model
-classification_vars <- data %>%
-  select(avg_rating, location, room_type, host_vs_superhost)
-
-# Prepare the data for modeling
-classification_vars <- as.data.frame(classification_vars)
-classification_vars <- mutate_all(classification_vars, as.factor)
+# Convert the Cluster column to a factor
+data_classification$Cluster <- as.factor(data_classification$Cluster)
 
 # Split the data into training and testing sets
 set.seed(123)
-train_index <- sample(nrow(classification_vars), nrow(classification_vars) * 0.8)
-train_data <- classification_vars[train_index, ]
-test_data <- classification_vars[-train_index, ]
+train_indices <- sample(1:nrow(data_classification), nrow(data_classification) * 0.8) # 80% for training
+train_data <- data_classification[train_indices, ]
+test_data <- data_classification[-train_indices, ]
 
-# Build the logistic regression model
-logit_model <- glmnet(avg_rating ~ location + room_type + host_vs_superhost,
-                      data = train_data,
-                      family = "binomial")
+# Train a decision tree model
+tree_model <- rpart(Cluster ~ Price + `Person Capacity` + `Cleanliness Rating` + Bedrooms + `City Center (km)` + `Attraction Index` + `Restraunt Index`, data = train_data, method = "class")
 
-# Predict the likelihood of high satisfaction ratings on the test data
-predictions <- predict(logit_model, newx = test_data, type = "response")
-predicted_classes <- ifelse(predictions > 0.5, "High_Satisfaction", "Not_High_Satisfaction")
+# Visualize the decision tree
+rpart.plot(tree_model, box.palette = "auto", shadow.col = "gray", nn = TRUE)
 
-# Evaluate the performance of
+# Make predictions on test data
+predictions <- predict(tree_model, test_data, type = "class")
+
+# Calculate accuracy
+accuracy <- sum(predictions == test_data$Cluster) / nrow(test_data)
+cat("Accuracy:", round(accuracy * 100, 2), "%")
+######################################################
+###############Forecasting############################
+######################################################
+
+# Create a copy of the original data
+data_forecast <- data
+
+# Convert satisfaction rating into categorical levels
+data_forecast$Satisfaction_Level <- ifelse(data_forecast$`Guest Satisfaction` >= 97, "High Satisfaction",
+                                          ifelse(data_forecast$`Guest Satisfaction` >= 80, "Satisfied", "Not Satisfied"))
+
+# Convert the satisfaction level column to a factor
+data_forecast$Satisfaction_Level <- as.factor(data_forecast$Satisfaction_Level)
+
+# Split the data into training and testing sets
+set.seed(123)
+train_indices <- sample(1:nrow(data_forecast), nrow(data_forecast) * 0.8) # 80% for training
+train_data <- data_forecast[train_indices, ]
+test_data <- data_forecast[-train_indices, ]
+
+# Train a decision tree model
+tree_model <- rpart(Satisfaction_Level ~ `Person Capacity` + `Cleanliness Rating` + `Restraunt Index`, 
+                   data = train_data, method = "class")
+
+# Visualize the decision tree
+rpart.plot(tree_model, box.palette = "auto", shadow.col = "gray", nn = TRUE)
+
+# Make predictions on test data
+predictions <- predict(tree_model, test_data, type = "class")
+
+# Calculate accuracy
+accuracy <- sum(predictions == test_data$Satisfaction_Level) / nrow(test_data)
+cat("Accuracy:", round(accuracy * 100, 2), "%")
